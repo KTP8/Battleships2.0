fdf9454f3ff0467ca4890822e25570e27ebe7465
KTP8 TP <ktaylorpatel@outlook.com>
2024-10-12 11:15:50 +0000

    Define all ship types & sizes

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+# Define ship types and their sizes
+SHIP_SIZES = {
+    "Carrier": 5,
+    "Battleship": 4,
+    "Cruiser": 3,
+    "Submarine": 3,
+    "Destroyer": 2
+}
